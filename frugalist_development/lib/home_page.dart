6a75a44b19c7cfcd829b58f1f7e9d05d0f7264b8
EyderACM <eyderacm@gmail.com>
2019-04-06 12:42:02 -0600

    add main layout

--- frugalist_development/lib/home_page.dart
+++ frugalist_development/lib/home_page.dart
@@ -5,8 +5,79 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello Home Page"),
+        child: Container(
+              padding: EdgeInsets.all(25),
+              constraints: BoxConstraints.expand(),
+              child: 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.start,              
+                  children: <Widget>[    
+                    Column(                      
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: 180,                          
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.only(top: 15),
+                          child: 
+                            Text(
+                              "¿Qué producto quieres buscar?",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 40,
+                                height: 1.1,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                        ),
+                        Container(
+                          width: 180,
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.only(top: 20),
+                          child: Text(
+                            "Mérida, Yucatán",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff49FE5B)
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    
+                    Container(
+                      width: 300,                  
+                      padding: EdgeInsets.only(top: 15),
+                      child: TextField(                    
+                        decoration: InputDecoration(
+                          hintText: "Busca"
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                          width: 180,
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.only(top: 20),
+                          child: Text(
+                            "¿Qué te gusta?",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff49FE5B)
+                            ),
+                          ),
+                        ),
+
+                    
+                  ],
+            ),
+        ),         
       ),
     );
   }
 }
+
+
