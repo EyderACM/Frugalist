4a20d97246fa65046b90ff24cd2ede5bac625294
Luis Leon <leon.luisgerardo775@gmail.com>
2019-05-09 01:30:34 -0500

    - Save image implemented at Backend

--- frugalist_tensorflow/src/app.js
+++ frugalist_tensorflow/src/app.js
@@ -1,11 +1,61 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+/**
+ * Express API
+ * @author Luis Gerardo Leon Ortega
+ *
+ * IDK if this is necessary but we can get request with JSON params.
+ * Maybe in a future to save data inside a database.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
+app.use(morgan('combined'));
 app.use(bodyParser.json());
+
+/*
+ * Code to implement storage in Express
+ */
+const Storage = multer.diskStorage({
+  destination: './images_ts',
+  filename: (req, file, cb) => {
+    //Generate a simple token to identify the image.
+    const fileName =
+      file.originalname.split('.')[0] +
+      '-' +
+      Date.now() +
+      path.extname(file.originalname);
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({
+  limits: 10 * 1024 * 1024,
+  storage: Storage,
+}).single('image');
+
+/**
+ * Routing
+ */
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    const p = req.file.path
+      .split(path.sep)
+      .slice(1)
+      .join('/');
+
+    res.status(200).json({ path: p });
+  });
+  /**
+   * Here we need implement TS image classificator.
+   */
+});
 
 app.listen(PORT, err => {
   if (err) {
