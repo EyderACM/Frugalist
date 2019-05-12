d2f69531d4e138815d4a7f986e64e7b75f691061
Luis Leon <leon.luisgerardo775@gmail.com>
2019-05-12 02:41:51 -0500

    - Shell code to configurate the cloud training

---
+++ shell_code.ps1
@@ -0,0 +1,7 @@
+'''
+@author: Luis Gerardo Leon Ortega
+
+Script of powershell to run machine learning
+'''
+$env:PROJECT_ID="sleek-ad28e"
+$env:GOOGLE_APPLICATION_CREDENTIALS="frugalist_tensorflow/key_frugalist.json"
